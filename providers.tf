9994435fc9c306c2c82059a57b9b5b7495f54807
destaben <estaben.sti@gmail.com>
2021-07-03 19:34:16 +0200

    New terraform version

--- providers.tf
+++ providers.tf
@@ -1,25 +1,40 @@
 provider "aws" {
-  version    = "~> 3.44.0"
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
-provider "null" {
-  version    = "~> 3.1.0"
-}
+provider "null" {}
 
-provider "archive" {
-  version    = "~> 2.2.0"
-}
+provider "archive" {}
 
 provider "github" {
   token   = var.github_token
   owner   = var.github_owner
-  version = "~> 4.12.1"
 }
 
 terraform {
+  required_version = "1.0.1" 
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.48.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.1.0"
+    }
+    archive = {
+      source = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "~> 4.12.1"
+    }
+  }
+
   backend "s3" {
     bucket         = var.bucket
     key            = var.key
