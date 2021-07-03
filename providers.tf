58b96a452b7f61587dbe6d3039db79b51b3c65c6
destaben <estaben.sti@gmail.com>
2021-07-03 19:12:14 +0200

    Style change

--- providers.tf
+++ providers.tf
@@ -21,11 +21,11 @@ provider "github" {
 
 terraform {
   backend "s3" {
-    bucket = var.bucket
-    key    = var.key
-    region = var.region
+    bucket         = var.bucket
+    key            = var.key
+    region         = var.region
     dynamodb_table = var.dynamodb_table
-    encrypt = true
-    versioning = true
+    encrypt        = true
+    versioning     = true
   }
 }
