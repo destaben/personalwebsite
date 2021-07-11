716e0fdf1b01bce53b83b2c30f87d7ba17326d25
destaben <estaben.sti@gmail.com>
2021-07-11 19:03:19 +0200

    Gzip and cache headers

--- infrastructure/frontend/cloudfront.tf
+++ infrastructure/frontend/cloudfront.tf
@@ -40,6 +40,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
+    compress               = true
   }
 
   restrictions {
